cba5477bab283ac0d6a3f939740cf6b726c12d67
Napat Srichan <mail@napatsc.com>
2023-04-26 13:13:16 -0700

    remove submodules

--- install.sh
+++ install.sh
@@ -3,5 +3,10 @@
 # move all config files to home dir
 rsync --exclude-from='.dockerignore' -avh . ~
 
+# clone 
+git clone git@github.com:romkatv/powerlevel10k.git .config/p10k
+git clone https://github.com/tmux-plugins/tpm.git \
+    .local/share/tmux/plugins/tpm
+
 # source it
 source ~/.profile
